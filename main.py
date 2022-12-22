09cc86fe8583ad0ee992a2b672fbdaa857a7d695
mrunknown6000 <dell06082009@gmail.com>
2022-12-22 20:33:35 +0700

    1.1 More User friendly stuff

--- main.py
+++ main.py
@@ -1,111 +1,20 @@
 from utils import *
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import selenium.common.exceptions as exc
-from selenium.webdriver.common.action_chains import ActionChains
 
-# FORMAT_TEST = r'C:\Users\admin\.src-code\python\Word2Quiz\content\scan-formatstyle.docx'
-LINE_TEST = r'C:\Users\admin\.src-code\python\Word2Quiz\content\scan-linebyline.docx'
-TABLE_TEST = r'C:\Users\admin\.src-code\python\Word2Quiz\content\scan-table.docx'
-GEO_CASE = r'C:\Users\admin\.src-code\python\Word2Quiz\content\testcasegeo.docx'
-
-# Networking Configuration
-driver = webdriver.Chrome()
-
-
-def main(doc_dir):
+def main(doc_dir, password, email):
     currentDocxString = convertDoc2Txt(doc_dir)
     currentDocxString = filterer(currentDocxString)
     finalizedDictionary = questionIdentification(currentDocxString)
-
-
-    global driver
-    driver.get("https://www.quizizz.com/login")
-    actions = ActionChains(driver)
-
-    # Login Screen -> Choose Email Option -> Create Quiz Screen
-    WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((
-            By.CLASS_NAME, 'email'
-        ))
-    )
-    driver.find_element(By.CLASS_NAME, 'email').click()
-    emailTxt = driver.find_element(By.NAME, "Enter email address or username")
-    passdTxt = driver.find_element(By.NAME, "Password")
-    emailTxt.clear()
-    passdTxt.clear()
-    emailTxt.send_keys("minh0608@outlook.com")
-    passdTxt.send_keys("minh0608")
-    try:
-        WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'continue-button'))
-        )
-        driver.find_element(By.CLASS_NAME, 'continue-button').click()
-
-        # Keyboard Navigate to Quiz Generate Area
-        # driver.implicitly_wait(4)
-        WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'admin-external-link'))
-        )
-        driver.find_element(By.CLASS_NAME, 'admin-external-link').click()
-
-        # ================= LOOP FOR EACH QUESTION ================
-        # Initial Loop hole
-        WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located(
-                (By.XPATH, '//*[@id="__layout"]/div/div[3]/main/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/button'))
-        )
-        driver.find_element(By.XPATH,
-                            '//*[@id="__layout"]/div/div[3]/main/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/button').click()
-
-        for questionStack in finalizedDictionary.values():
-            # Actually Process all the question
-            WebDriverWait(driver, 30).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, '//*[@id="questionQuery"]'))
-            )
-
-            # Interact
-            driver.find_element(By.XPATH, '//*[@id="questionQuery"]').click()
-            actions.send_keys(questionStack['title'])
-            actions.perform()
-            for i in range(len(questionStack['options'])):
-                driver.find_element(By.XPATH, f'//*[@id="option-{i}"]').click()
-                actions.send_keys(questionStack['options'][i])
-                actions.perform()
-                if questionStack['options'][i] == questionStack['answer']:
-                    driver.find_element(By.XPATH,
-                                        f'//*[@id="__layout"]/div/div[3]/div/div/div/div/div/div/div[2]/div[1]/div/div[3]/div/ul/li[{i + 1}]/div/div[1]/div[3]/button').click()
-            # Save Button
-            driver.find_element(By.XPATH,
-                                '//*[@id="__layout"]/div/div[3]/div/div/div/div/div/div/div[2]/div[2]/div[1]/div/button').click()
-            # New Question Wizard
-            if questionStack['title'] == finalizedDictionary[len(finalizedDictionary) - 1]['title']:
-                break
-            WebDriverWait(driver, 30).until(
-                EC.presence_of_element_located(
-                    (By.CLASS_NAME, 'quick-add-action'))
-            )
-            driver.execute_script('arguments[0].click();', driver.find_element(By.CLASS_NAME, 'quick-add-action'))
-            WebDriverWait(driver, 30).until(
-                EC.presence_of_element_located((
-                    By.XPATH, '//*[@id="__layout"]/div/div[3]/div/div/div/div/div[2]/div[1]/button'
-                ))
-            )
-            driver.execute_script('arguments[0].click();', driver.find_element(By.XPATH,
-                                                                               '//*[@id="__layout"]/div/div[3]/div/div/div/div/div[2]/div[1]/button'))
-
-        breakpoint()
-    #     USER INPUT
-    except exc.TimeoutException:
-        print("Connection got time out ;-;")
+    driver = webdriver.Chrome()
+    connectionApplier(driver, finalizedDictionary, email, password)
 
 
 if __name__ == '__main__':
-    wordInput = r'C:\Users\admin\.src-code\python\Word2Quiz\geo.docx'
-    main(wordInput)
+    wordInput = input("Gõ đường dẫn file Word: ")
+    emailInp = input("Gõ email tài khoản Quizzi của bạn: ")
+    passdInp = input("Gõ mật khẩu tài khoảng Quizzi của bạn: ")
+
+    # main(wordInput, passdInp, emailInp)
 
 """
 TODO MAIN LIST:
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import docx2txt
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import selenium.common.exceptions as exc
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 def convertDoc2Txt(doc_direct: str) -> str:
@@ -6,10 +11,10 @@ def convertDoc2Txt(doc_direct: str) -> str:
         text = docx2txt.process(doc_direct)
         return text
     except PermissionError:
-        print("Error: File Access Denial!")
+        print("Lỗi | Truy cập bị từ chối")
         exit(1)
     except FileNotFoundError:
-        print("Error: File Not Found")
+        print("Lỗi | File không tồn tại")
         exit(1)
 
 
@@ -71,9 +76,90 @@ def questionIdentification(formatted_string: str) -> dict:
             finalized[questionId]["answer"] = answerList[questionId]
             finalized[questionId]["options"] = optionList[questionId]
     except IndexError:
-        print("Error: Failed to filtering the file")
+        print("Lỗi | Lỗi ký hiệu, kiểm tra lại file")
 
         # print(titleList)
         # print(optionList)
     return finalized
 
+
+def connectionApplier(driver, questions_dictionary, email, password):
+    driver.get("https://www.quizizz.com/login")
+    actions = ActionChains(driver)
+
+    # Login Screen -> Choose Email Option -> Create Quiz Screen
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((
+            By.CLASS_NAME, 'email'
+        ))
+    )
+    driver.find_element(By.CLASS_NAME, 'email').click()
+    emailTxt = driver.find_element(By.NAME, "Enter email address or username")
+    passdTxt = driver.find_element(By.NAME, "Password")
+    emailTxt.clear()
+    passdTxt.clear()
+    emailTxt.send_keys(email)
+    passdTxt.send_keys(password)
+    try:
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'continue-button'))
+        )
+        driver.find_element(By.CLASS_NAME, 'continue-button').click()
+
+        # Keyboard Navigate to Quiz Generate Area
+        # driver.implicitly_wait(4)
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'admin-external-link'))
+        )
+        driver.find_element(By.CLASS_NAME, 'admin-external-link').click()
+
+        # ================= LOOP FOR EACH QUESTION ================
+        # Initial Loop hole
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="__layout"]/div/div[3]/main/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/button'))
+        )
+        driver.find_element(By.XPATH,
+                            '//*[@id="__layout"]/div/div[3]/main/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/button').click()
+
+        for questionStack in questions_dictionary.values():
+            # Actually Process all the question
+            WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="questionQuery"]'))
+            )
+
+            # Interact
+            driver.find_element(By.XPATH, '//*[@id="questionQuery"]').click()
+            actions.send_keys(questionStack['title'])
+            actions.perform()
+            for i in range(len(questionStack['options'])):
+                driver.find_element(By.XPATH, f'//*[@id="option-{i}"]').click()
+                actions.send_keys(questionStack['options'][i])
+                actions.perform()
+                if questionStack['options'][i] == questionStack['answer']:
+                    driver.find_element(By.XPATH,
+                                        f'//*[@id="__layout"]/div/div[3]/div/div/div/div/div/div/div[2]/div[1]/div/div[3]/div/ul/li[{i + 1}]/div/div[1]/div[3]/button').click()
+            # Save Button
+            driver.find_element(By.XPATH,
+                                '//*[@id="__layout"]/div/div[3]/div/div/div/div/div/div/div[2]/div[2]/div[1]/div/button').click()
+            # New Question Wizard
+            if questionStack['title'] == questions_dictionary[len(questions_dictionary) - 1]['title']:
+                break
+            WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, 'quick-add-action'))
+            )
+            driver.execute_script('arguments[0].click();', driver.find_element(By.CLASS_NAME, 'quick-add-action'))
+            WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((
+                    By.XPATH, '//*[@id="__layout"]/div/div[3]/div/div/div/div/div[2]/div[1]/button'
+                ))
+            )
+            driver.execute_script('arguments[0].click();', driver.find_element(By.XPATH,
+                                                                               '//*[@id="__layout"]/div/div[3]/div/div/div/div/div[2]/div[1]/button'))
+
+        breakpoint()
+    #     USER INPUT
+    except exc.TimeoutException:
+        print("Lỗi | Mạng chậm quá .-.")
